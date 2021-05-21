c698cf49639041a86e3372e45ea317ba7c98e2a6
ryznocaHub <fikrihalim27@gmail.com>
2021-05-22 03:14:17 +0700

    add red color

--- src/assets/color.js
+++ src/assets/color.js
@@ -1,10 +1,11 @@
 export const Color = {
-    white: '#fff',
-    black: '#000',
-    primary : '#9c88ff',
-    secondary: '#8395a7',
-    blackDoff: '#2B2B2B',
-    secondaryPrimary: '#d6d6d6',
-    softGray: '#F2F2F2',
-    blackLight: '#444444',
-}
+  white: "#fff",
+  black: "#000",
+  primary: "#9c88ff",
+  secondary: "#8395a7",
+  blackDoff: "#2B2B2B",
+  secondaryPrimary: "#d6d6d6",
+  softGray: "#F2F2F2",
+  blackLight: "#444444",
+  red: "#ff6b6b",
+};

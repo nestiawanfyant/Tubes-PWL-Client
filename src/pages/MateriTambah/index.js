0e92b3589a8ffab89787e776f3a343624c05b66c
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-05-25 01:45:52 +0700

    update page MateriTambah

--- src/pages/MateriTambah/index.js
+++ src/pages/MateriTambah/index.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Col, Form, InputGroup, Row } from "react-bootstrap";
 //bootstra
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -7,10 +6,6 @@ import { Button } from "react-bootstrap";
 import { createUseStyles } from "react-jss";
 //assets
 import { Color } from "../../assets/color";
-//components
-import { CardKelas } from "../../components";
-//icon
-import { FiFileText, FiX } from "react-icons/fi";
 
 const MateriTambah = ({ slug }) => {
   const classes = styles();

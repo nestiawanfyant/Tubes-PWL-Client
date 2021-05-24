cbf13f813c130f65a98637d99ca470b5a76d44a1
ryznocaHub <fikrihalim27@gmail.com>
2021-05-25 06:07:52 +0700

    add modal tambah tugas

--- src/pages/ViewTugas/index.js
+++ src/pages/ViewTugas/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { createUseStyles } from "react-jss";
 // bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Accordion, Card } from "react-bootstrap";
+import { Accordion, Card, Modal } from "react-bootstrap";
 // assets
 import { Color } from "../../assets/color";
 import "../../assets/css/font.css";
@@ -12,45 +12,67 @@ import { BiPlus } from "react-icons/bi";
 import { CardListTugas } from "../../components";
 // page for route
 import { TugasTambah } from "../../pages";
-import { useParams } from 'react-router-dom'
+import { useParams } from "react-router-dom";
 
 const ViewTugas = () => {
   const styles = style();
-  const { name } = useParams()
-  const [tugas, setTugas] = useState([])
+  const { name } = useParams();
+  const [tugas, setTugas] = useState([]);
+  const [show, setShow] = useState(false);
 
+  const handleShow = () => setShow(true);
+  const handleClose = () => {
+    setShow(false);
+  };
   useEffect(() => {
-    fetch('http://127.0.0.1:8000/tugas/list?' + 'slug=' + name)
-      .then(response => response.json())
-      .then(responseJson => {
-        setTugas(responseJson)
+    fetch("http://127.0.0.1:8000/tugas/list?" + "slug=" + name)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setTugas(responseJson);
       })
-      .catch(e => console.log(e));
-  }, [tugas])
+      .catch((e) => console.log(e));
+  }, [tugas]);
 
   return (
     <div>
       <Accordion className={styles.accordion}>
         <Card>
-          <Accordion.Toggle as={Card.Header} eventKey="0">
+          <Accordion.Toggle as={Card.Header} onClick={handleShow}>
             <div className={styles.iconBox}>
               Tambah Tugas <BiPlus className={styles.icon} />
             </div>
           </Accordion.Toggle>
-          <Accordion.Collapse eventKey="0">
-            <Card.Body>
-              <TugasTambah slug={name} />
-            </Card.Body>
-          </Accordion.Collapse>
         </Card>
       </Accordion>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Tambah Tugas</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <TugasTambah slug={name} />
+        </Modal.Body>
+      </Modal>
       <br />
-      {
-        tugas.length > 0 ? tugas.map(data => {
-          return (<><CardListTugas key={data.id} nama={data.nama} user={data.user.nama} deskripsi={data.deskripsi} id={data.id} deadline={data.deadline} slug={data.slug} /> <br /></>)
-        }) :
-          <h3>Belum ada tugas</h3>
-      }
+      {tugas.length > 0 ? (
+        tugas.map((data) => {
+          return (
+            <>
+              <CardListTugas
+                key={data.id}
+                nama={data.nama}
+                user={data.user.nama}
+                deskripsi={data.deskripsi}
+                id={data.id}
+                deadline={data.deadline}
+                slug={data.slug}
+              />{" "}
+              <br />
+            </>
+          );
+        })
+      ) : (
+        <h3>Belum ada tugas</h3>
+      )}
     </div>
   );
 };
@@ -70,7 +92,6 @@ const style = createUseStyles({
     alignItems: "center",
     color: Color.primary,
     fontSize: 20,
-
   },
   icon: {
     marginLeft: 10,

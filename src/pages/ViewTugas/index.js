37e912864e3662886b00bd42c38cfa47a3d37922
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-05-24 16:33:54 +0700

    update page ViewTugas

--- src/pages/ViewTugas/index.js
+++ src/pages/ViewTugas/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createUseStyles } from "react-jss";
 // bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -12,9 +12,21 @@ import { BiPlus } from "react-icons/bi";
 import { CardListTugas } from "../../components";
 // page for route
 import { TugasTambah } from "../../pages";
+import { useParams } from 'react-router-dom'
 
 const ViewTugas = () => {
   const styles = style();
+  const { name } = useParams()
+  const [tugas, setTugas] = useState([])
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/tugas/list?' + 'slug=' + name)
+      .then(response => response.json())
+      .then(responseJson => {
+        setTugas(responseJson)
+      })
+      .catch(e => console.log(e));
+  }, [tugas])
 
   return (
     <div>
@@ -27,15 +39,18 @@ const ViewTugas = () => {
           </Accordion.Toggle>
           <Accordion.Collapse eventKey="0">
             <Card.Body>
-              <TugasTambah />
+              <TugasTambah slug={name} />
             </Card.Body>
           </Accordion.Collapse>
         </Card>
       </Accordion>
       <br />
-      <CardListTugas/> <br />
-      <CardListTugas/> <br />
-      <CardListTugas/> <br />
+      {
+        tugas.length > 0 ? tugas.map(data => {
+          return (<><CardListTugas key={data.id} nama={data.nama} user={data.user.nama} deskripsi={data.deskripsi} id={data.id} deadline={data.deadline} slug={data.slug} /> <br /></>)
+        }) :
+          <h3>Belum ada tugas</h3>
+      }
     </div>
   );
 };

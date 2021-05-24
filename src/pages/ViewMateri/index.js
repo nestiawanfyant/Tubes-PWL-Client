9731eff9a8956282e4b351c1dc724f1b4ccf4359
ryznocaHub <fikrihalim27@gmail.com>
2021-05-25 06:08:23 +0700

    add modal tambah materi

--- src/pages/ViewMateri/index.js
+++ src/pages/ViewMateri/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { createUseStyles } from "react-jss";
 // bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Accordion, Card } from "react-bootstrap";
+import { Accordion, Card, Modal } from "react-bootstrap";
 // assets
 import { Color } from "../../assets/color";
 import "../../assets/css/font.css";
@@ -12,46 +12,67 @@ import { BiPlus } from "react-icons/bi";
 import { CardListMateri } from "../../components";
 // page for route
 import { MateriTambah } from "../../pages";
-import { useParams } from 'react-router-dom'
+import { useParams } from "react-router-dom";
 
 const ViewMateri = () => {
   const styles = style();
-  const { name } = useParams()
-  const [materi, setMateri] = useState([])
+  const { name } = useParams();
+  const [materi, setMateri] = useState([]);
+  const [show, setShow] = useState(false);
+
+  const handleShow = () => setShow(true);
+  const handleClose = () => {
+    setShow(false);
+  };
 
   useEffect(() => {
-    fetch('http://127.0.0.1:8000/materi/list?' + 'slug=' + name)
-      .then(response => response.json())
-      .then(responseJson => {
-        setMateri(responseJson)
+    fetch("http://127.0.0.1:8000/materi/list?" + "slug=" + name)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setMateri(responseJson);
       })
-      .catch(e => console.log(e));
-  }, [materi])
+      .catch((e) => console.log(e));
+  }, [materi]);
 
   return (
     <div>
       <Accordion className={styles.accordion}>
         <Card>
-          <Accordion.Toggle as={Card.Header} eventKey="0">
+          <Accordion.Toggle as={Card.Header} onClick={handleShow}>
             <div className={styles.iconBox}>
               Tambah Materi <BiPlus className={styles.icon} />
             </div>
           </Accordion.Toggle>
-          <Accordion.Collapse eventKey="0">
-            <Card.Body>
-              <MateriTambah slug={name} />
-            </Card.Body>
-          </Accordion.Collapse>
         </Card>
       </Accordion>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Tambah Materi</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <MateriTambah slug={name} />
+        </Modal.Body>
+      </Modal>
       <br />
-      {
-        materi.length > 0 ? materi.map(data => {
-          return (<><CardListMateri key={data.id} nama={data.nama} user={data.user.nama} deskripsi={data.deskripsi} id={data.id} slug={data.slug} /> <br /></>)
-        }) :
-          <h3>Belum ada materi</h3>
-      }
-
+      {materi.length > 0 ? (
+        materi.map((data) => {
+          return (
+            <>
+              <CardListMateri
+                key={data.id}
+                nama={data.nama}
+                user={data.user.nama}
+                deskripsi={data.deskripsi}
+                id={data.id}
+                slug={data.slug}
+              />{" "}
+              <br />
+            </>
+          );
+        })
+      ) : (
+        <h3>Belum ada materi</h3>
+      )}
     </div>
   );
 };
@@ -71,7 +92,6 @@ const style = createUseStyles({
     alignItems: "center",
     color: Color.primary,
     fontSize: 20,
-
   },
   icon: {
     marginLeft: 10,

666b275d707ce35b6c7ff49654afb25bac557e73
Nazla Andintya Wijaya <nazla.118140084@student.itera.ac.id>
2021-05-25 10:48:20 +0700

    add deskripsi profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -48,12 +48,15 @@ const Profile = () => {
           roundedCircle
         />
 
-    <br />
-    <br />
-
-    
     <div className={styles.deskripsi}>
-
+      <h2> Deskripsi </h2> 
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a 
+        type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining 
+        essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum 
+        passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      </p> 
     </div>
 
     </div>
@@ -72,7 +75,7 @@ const style = createUseStyles({
     height: 120,
     position: 'relative',
     top: "-90px",
-    left: "3x0px",
+    left: "30px",
   },
   text: {
     color: Color.primary,
